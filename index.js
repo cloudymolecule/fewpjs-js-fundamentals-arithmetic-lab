70910f9524b2a2b0b2fc13538678d3e5d0867a06
Cloudymolecule <baires82@gmail.com>
2021-02-24 10:27:54 -0500

    commit tree 631dc8ffa12adf7ec9d4223c245273aef2a55f76

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 10
+let num2 = 6.2
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 14
+let num4 = 5
+
+let mod = num3 % num4
+
+let max = Math.max(19, 20)
